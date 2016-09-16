abb08a0fc0b27f4c7b0585298730fdbaa9f119a2
Northard, Robert A <robert.a.northard@accenture.com>
2016-09-16 13:00:59 +0100

    Added adop-default credentials.

--- projects/groovy/create_default_credentials.groovy
+++ projects/groovy/create_default_credentials.groovy
@@ -6,15 +6,18 @@ import com.cloudbees.plugins.credentials.impl.*
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
 import hudson.plugins.sshslaves.*;
 
-def domain = Domain.global()
-def store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+def add_username_password_credential(id, username, password, description){
+  def domain = Domain.global()
+  def store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
 
-def defaultUsernameAndPassword = new UsernamePasswordCredentialsImpl(
-  CredentialsScope.GLOBAL,
-  "adop-default",
-  "Default credentials for Jenkins. This credential does not grant access to any service.",
-  "adop-default",
-  "adop-default"
-)
+  def defaultUsernameAndPassword = new UsernamePasswordCredentialsImpl(
+    CredentialsScope.GLOBAL,
+    id,
+    description,
+    username,
+    password
+  )
+  store.addCredentials(domain, defaultUsernameAndPassword)
+}
 
-store.addCredentials(domain, defaultUsernameAndPassword)
+add_username_password_credential("adop-default","adop-default", "adop-default","Default credentials for Jenkins. This credential does not grant access to any service." )
