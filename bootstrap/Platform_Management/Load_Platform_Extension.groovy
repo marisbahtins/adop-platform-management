568b18ff7cc51d3817027d226a7d4d41a3c307d7
Robert Northard <robertnorthard@googlemail.com>
2016-09-16 13:13:19 +0100

    Added creation of default user for build param placeholder.

--- bootstrap/Platform_Management/Load_Platform_Extension.groovy
+++ bootstrap/Platform_Management/Load_Platform_Extension.groovy
@@ -4,7 +4,7 @@ def platformManagementFolder = folder(platformManagementFolderName) { displayNam
 
 // Jobs
 def loadPlatformExtensionJob = freeStyleJob(platformManagementFolderName + "/Load_Platform_Extension")
- 
+
 // Setup setup_cartridge
 loadPlatformExtensionJob.with{
     wrappers {
@@ -12,12 +12,12 @@ loadPlatformExtensionJob.with{
         sshAgent('adop-jenkins-master')
     }
     parameters{
-      stringParam("GIT_URL",'',"The URL of the git repo for Platform Extension")
-      stringParam("GIT_REF","master","The reference to checkout from git repo of Platform Extension. It could be a branch name or a tag name. Eg : master, 0.0.1 etc")
-      credentialsParam("AWS_CREDENTIALS"){
-        type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
-        description('AWS access key and secret key for your account')
-      }
+        stringParam('COLLECTION_URL', '', 'URL to a JSON file defining your platform extension collection.')
+        credentialsParam("CREDENTIALS"){
+            type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
+            defaultValue('adop-default')
+            description('Platform extension credentials. Note: Leave at adop-default if credentials are not required.')
+        }
     }
     scm{
       git{
@@ -35,7 +35,7 @@ loadPlatformExtensionJob.with{
       maskPasswords()
       sshAgent("adop-jenkins-master")
       credentialsBinding {
-        usernamePassword("AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", '${AWS_CREDENTIALS}')
+        usernamePassword("USERNAME", "PASSWORD", '${CREDENTIALS}')
       }
     }
     steps {
@@ -50,12 +50,15 @@ fi
 
 # Provision any EC2 instances in the AWS folder
 if [ -d ${WORKSPACE}/service/aws ]; then
-    
+
     if [ -f ${WORKSPACE}/service/aws/service.template ]; then
-        
+
         echo "#######################################"
         echo "Adding EC2 platform extension on AWS..."
-        
+
+        export AWS_ACCESS_KEY_ID=${USERNAME}
+        export AWS_SECRET_ACCESS_KEY=${PASSWORD}
+
         # Variables
         INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
         PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
@@ -68,10 +71,10 @@ if [ -d ${WORKSPACE}/service/aws ]; then
             AWS_VPC_ID=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID}           --query 'Reservations[0].Instances[0].VpcId' --output text);
         fi
         CIDR_BLOCK=$(aws ec2 describe-vpcs --vpc-ids ${AWS_VPC_ID} --query 'Vpcs[0].CidrBlock' --output text)
-      
+
         ENVIRONMENT_STACK_NAME="${AWS_VPC_ID}-EC2-PLATFORM-EXTENSION-${PLATFORM_EXTENSION_NAME}-${BUILD_NUMBER}"
         FULL_ENVIRONMENT_NAME="${AWS_VPC_ID}-EC2-Instance-${PLATFORM_EXTENSION_NAME}-${BUILD_NUMBER}"
-        
+
         aws cloudformation create-stack --stack-name ${ENVIRONMENT_STACK_NAME} \
         --tags "Key=createdBy,Value=ADOP-Jenkins" "Key=user,Value=${INITIAL_ADMIN_USER}" \
         --template-body file://service/aws/service.template \
@@ -81,7 +84,7 @@ if [ -d ${WORKSPACE}/service/aws ]; then
         ParameterKey=KeyName,ParameterValue=${AWS_KEYPAIR} \
         ParameterKey=VPCId,ParameterValue=${AWS_VPC_ID} \
         ParameterKey=InboundCIDR,ParameterValue=${CIDR_BLOCK}
-            
+
         # Keep looping whilst the stack is being created
         SLEEP_TIME=60
         COUNT=0
@@ -93,7 +96,7 @@ if [ -d ${WORKSPACE}/service/aws ]; then
             sleep "${SLEEP_TIME}"
             COUNT=$((COUNT+1))
         done
-        
+
         # Check that the stack created
         TIME_SPENT=$(($COUNT * $SLEEP_TIME))
         if $(aws cloudformation describe-stacks --stack-name ${ENVIRONMENT_STACK_NAME} | grep -q "CREATE_COMPLETE")
@@ -105,7 +108,7 @@ if [ -d ${WORKSPACE}/service/aws ]; then
             echo "ERROR : Stack creation failed after ${TIME_SPENT} seconds. Please check the AWS console for more information."
             exit 1
         fi
-        
+
         echo "Success! The private IP of your new EC2 instance is $NODE_IP"
         echo "Please use your provided key, ${AWS_KEYPAIR}, in order to SSH onto the instance."
 
@@ -119,7 +122,7 @@ if [ -d ${WORKSPACE}/service/aws ]; then
             sleep "${SLEEP_TIME}"
             COUNT=$((COUNT+1))
         done
-        
+
         # Check that the instance has initalized and all tests have passed
         TIME_SPENT=$(($COUNT * $SLEEP_TIME))
         if $(aws ec2 describe-instance-status --instance-ids ${NEW_INSTANCE_ID} --query 'InstanceStatuses[0].InstanceStatus' --output text | grep -q "passed")
@@ -133,26 +136,26 @@ if [ -d ${WORKSPACE}/service/aws ]; then
 
 
         if [ -f ${WORKSPACE}/service/aws/ec2-extension.conf ]; then
-                        
+
             echo "#######################################"
             echo "Adding EC2 instance to NGINX config using xip.io..."
-            
+
             export SERVICE_NAME="EC2-Service-Extension-${BUILD_NUMBER}"
             cp ${WORKSPACE}/service/aws/ec2-extension.conf ec2-extension.conf
-            
+
             ## Add nginx configuration
             sed -i "s/###EC2_SERVICE_NAME###/${SERVICE_NAME}/" ec2-extension.conf
             sed -i "s/###EC2_HOST_IP###/${NODE_IP}/" ec2-extension.conf
             docker cp ec2-extension.conf proxy:/etc/nginx/sites-enabled/${SERVICE_NAME}.conf
-            
+
             ## Reload nginx
             docker exec proxy /usr/sbin/nginx -s reload
-            
+
             echo "You can check that your EC2 instance has been succesfully proxied by accessing the following URL: ${SERVICE_NAME}.${PUBLIC_IP}.xip.io"
         else
             echo "INFO: /service/aws/ec2-extension.conf not found"
         fi
-        
+
     else
         echo "INFO: /service/aws/service.template not found"
     fi
